89802a0b48b38b69caf2d6cf279fa7b088312cb5
lucasquetinho <105065904+lucasquetinho@users.noreply.github.com>
2024-06-17 08:53:02 -0300

    Aulas 7 e 8 feitas

--- script.js
+++ script.js
@@ -6,43 +6,103 @@ const textoResultado = Document.querySelector(".texto-Resultado");
 
 const Perguntas = [
     {
-    enunciado: "João após passar por um lago, reparou que uma parte dele não era água, após perguntar ao seu pai ele descobriu que aquilo verde no lago eram algas invasoras. Você sabe o que são algas invasoras, e se são boas para a vida aquática?",
-    Alternativas: [
-            "Sim",
-            "Não"
+    enunciado: "olá",
+    alternativas: [
+        {
+            texto:"Isso é assustador",
+            afirmação: "afirmacao"
+
+        },
+        {
+            texto:"Isso é assustador",
+            afirmação: "afirmacao"
+
+        }
         ]
 },
 
 {
-    enunciado: "Mesmo ja sido repondido, João ainda se perguntava por que aquilo ocorria, até que ele avistou um guarda e foi perguntar. Então o guarda respondeu que aquilo acontece quando á:",
-    Alternativas: [
-            "Dematamento das margens dos rios",
-            "Erosão das margens dos rios",
-            "Excesso de nutrientes na água(Correta)",
-            "Secas anuais",
-            "Alteração constante na água"
+    enunciado: "Mesmo ja  tendo sido repondido, João ainda se perguntava por que aquilo ocorria, até que ele avistou um guarda e foi perguntar. Então o guarda respondeu que aquilo acontece quando á:",
+    alternativas: [
+        {
+            texto:"Isso é assustador",
+            afirmação: "afirmacao"
+
+        },
+        {
+            texto:"Isso é assustador",
+            afirmação: "afirmacao"
+
+        }
         ]
 },
 {
     enunciado: "Quais são as consequências da proliferação?",
-    Alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+    alternativas: [
+        {
+            texto:"Isso é assustador",
+            afirmação: "afirmacao"
+
+        },
+        {
+            texto:"Isso é assustador",
+            afirmação: "afirmacao"
+
+        }
         ]
 },
 
 {
     enunciado: "Existe alguma forma de prevenir",
-    Alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+    alternativas: [
+        {
+        texto:"Isso é assustador",
+        afirmação: "afirmacao"
+
+    },
+    {
+        texto:"Isso é assustador",
+        afirmação: "afirmacao"
+
+    }
         ]
 },
 {
-    enunciado: "Quais medidas podem ser tomadas para controlar?",
-    Alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
+    enunciado: "Quais medid[as podem ser tomadas para controlar?",
+    alternativas: [
+        {
+        texto:"Isso é assustador",
+        afirmação: "afirmacao"
+
+    },
+    {
+        texto:"Isso é assustador",
+        afirmação: "afirmacao"
+
+    }
+    ]
 }
-]
+];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
+
+mostraPergunta();
