116b6820930c57991b05e221b48750f7f37d1e4a
lucasquetinho <105065904+lucasquetinho@users.noreply.github.com>
2024-09-02 22:19:54 -0300

    Delete trabalho-missao-main directory

--- trabalho-missao-main/js/aleatorio.js
+++
@@ -1,8 +0,0 @@
-const nomes = ["Pedro", "Fernanda", "João", "Marcelo", "Paula", "Joana"];
-
-export function aleatorio(lista) {
-    const posicao = Math.floor(Math.random() * lista.length);
-    return lista[posicao];
-}
-
-export const nome = aleatorio(nomes);
